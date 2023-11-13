0edaab1bb6502d1becd7eafec2e99c51a9ba94bf
kstanis <ssk-90@tut.by>
2023-11-13 16:21:01 +0100

    upd jenkins

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
 
-    return "<p>Hello, Lenkinsы!</p>"
+    return "<p>Hello, jenkins!</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
